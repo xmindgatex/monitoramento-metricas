8d597d27227c718117ffeeb776d2ccb640399059
kaio pinheiro <138056146+xmindgatex@users.noreply.github.com>
2024-11-20 18:18:37 -0300

    Update MetricsGeneratorServiceTest.java

--- blackend/scr/src/test/java/com/monitoramento/MetricsGeneratorServiceTest.java
+++ blackend/scr/src/test/java/com/monitoramento/MetricsGeneratorServiceTest.java
@@ -1,1 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+public class MetricsGeneratorServiceTest {
+    @Test
+    public void testMetricGeneration() {
+        MetricsGeneratorService service = new MetricsGeneratorService();
+        Metric metric = service.generateMetrics();
+        
+        assertTrue(metric.getCpu() >= 0 && metric.getCpu() <= 100);
+        assertTrue(metric.getMemory() >= 0 && metric.getMemory() <= 100);
+        assertTrue(metric.getLatency() >= 50 && metric.getLatency() <= 500);
+    }
+}
